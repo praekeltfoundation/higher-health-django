8677adb7fe17a0e96e92e967c96aaa1c36cb117c
Jacques Coetzee <j.coetzee0@gmail.com>
2020-05-05 19:33:58 +0200

    update form

--- higher_health/forms.py
+++ higher_health/forms.py
@@ -1,6 +1,51 @@
 from django import forms
 from django.forms.widgets import PasswordInput, TextInput
+from django.utils.translation import ugettext_lazy as _
 
-class SimpleForm(forms.Form):
-    firstname = forms.CharField(widget=TextInput(attrs={'placeholder': 'First name'}),max_length=100)
-    lastname = forms.CharField(widget=TextInput(attrs={'placeholder': 'Last name'}),max_length=100)
+class CheckerForm(forms.Form):
+    ageRange = forms.ChoiceField(
+        label="How old are you?",
+        choices= (
+            ('', _('Age')),
+            ('<18', _('<18')),
+            ('18-39', _('18-39')),
+            ('40-65', _('40-65')),
+            ('>65', _('>65'))
+        ),
+        required=True
+    )
+    gender = forms.ChoiceField(
+        label="Please provide us with the gender you identify as",
+        choices= (
+            ('', _('Gender')),
+            ('Male', _('Male')),
+            ('Female', _('Female')),
+            ('Other', _('Other')),
+            ('Rather not say', _('Rather not say'))
+        ),
+        required=True
+    )
+    province = forms.ChoiceField(
+        label="In which Province are you currently residing?",
+        choices= (
+            ('', _('Province')),
+            ('Eastern Cape', _('Eastern Cape')),
+            ('Free State', _('Free State')),
+            ('Gauteng', _('Gauteng')),
+            ('KwaZulu Natal', _('KwaZulu Natal')),
+            ('Limpopo', _('Limpopo')),
+            ('Mpumalanga', _('Mpumalanga')),
+            ('North West', _('North West')),
+            ('Northern Cape', _('Northern Cape')),
+            ('Western Cape', _('Western Cape')),
+        ),
+        required=False
+    )
+    provinceType = forms.CharField(widget=TextInput(attrs={'class':'textInputs','placeholder': 'Which Province are you currently residing?'}),max_length=100)
+    residenceLocation = forms.CharField(widget=TextInput(attrs={'class':'textInputs','placeholder': 'Name of your Suburb, Township, Town or Village (or nearest)'}),max_length=100)
+    locationPin = forms.CharField(widget=TextInput(attrs={'class':'textInputs','placeholder': 'Please share your location using (Or SKIP if you are unable to)','helpText':'This will help us to more accurately map cases of COVID-19'}),max_length=100)
+
+
+class LoginForm(forms.Form):
+    firstname = forms.CharField(widget=TextInput(attrs={'class':'textInputs','placeholder': 'First name'}),max_length=100)
+    lastname = forms.CharField(widget=TextInput(attrs={'class':'textInputs','placeholder': 'Last name'}),max_length=100)
--- higher_health/views.py
+++ higher_health/views.py
@@ -1,9 +1,9 @@
-from .forms import SimpleForm
+from .forms import CheckerForm
 from django.shortcuts import render
 
 def index(request):
 	return render(request, 'base.html')
 
 def form_new(request):
-    form = SimpleForm()
+    form = CheckerForm()
     return render(request, 'form_edit.html', {'form': form})
