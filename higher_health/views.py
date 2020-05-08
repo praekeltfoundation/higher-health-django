204136bb192b87d54fbfa89f1ffec1e2c0c56d84
Erik Harding <erik@praekelt.org>
2020-05-08 11:55:42 +0200

    black and isort

--- higher_health/views.py
+++ higher_health/views.py
@@ -1,33 +1,35 @@
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from django.http import HttpResponseRedirect
-from django.core.exceptions import ValidationError
-from .forms import HealthCheckQuestionnaire, HealthCheckLogin
-
+from .forms import HealthCheckLogin, HealthCheckQuestionnaire
 
 
 def healthcheck_questionnaire(request):
     submitted = False
-    if request.method == 'POST':
+    if request.method == "POST":
         form = HealthCheckQuestionnaire(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             # assert False
-            return HttpResponseRedirect('/?submitted=True')
+            return HttpResponseRedirect("/?submitted=True")
     else:
         form = HealthCheckQuestionnaire()
-        if 'submitted' in request.GET:
+        if "submitted" in request.GET:
             submitted = True
 
-    return render(request, "healthcheck_questionnaire.html", {"form": form, "submitted": submitted })
+    return render(
+        request,
+        "healthcheck_questionnaire.html",
+        {"form": form, "submitted": submitted},
+    )
+
 
 def healthcheck_login(request):
     form = HealthCheckLogin()
     return render(request, "healthcheck_login.html", {"form": form})
 
-def healthcheck_terms(
-        request,
-        extra_context=None,
-        template=('healthcheck_terms.html')):
-    locale_code = request.GET.get('locale')
-    return render(request, template, {'locale_code': locale_code})
+
+def healthcheck_terms(request, extra_context=None, template=("healthcheck_terms.html")):
+    locale_code = request.GET.get("locale")
+    return render(request, template, {"locale_code": locale_code})
