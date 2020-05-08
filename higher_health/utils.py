279fb9591fd4ebf1fef09f1f2463400a1c9ebb92
Erik Harding <erik@praekelt.org>
2020-05-08 16:23:41 +0200

    create model and save data

--- higher_health/utils.py
+++ higher_health/utils.py
@@ -1,3 +1,6 @@
+from higher_health.models import Covid19Triage
+
+
 def get_risk_level(data):
     symptoms = 0
     for key, value in data.items():
@@ -23,3 +26,39 @@ def get_risk_level(data):
             return "low"
 
     return "not_sure"
+
+
+def get_location(data):
+    lat = data["latitude"]
+    lng = data["longitude"]
+
+    if "-" not in lat:
+        lat = f"+{lat}"
+
+    if "-" not in lng:
+        lng = f"+{lng}"
+
+    return f"{lat}{lng}"
+
+
+def save_data(data, risk_level):
+    print(data)
+    return Covid19Triage.objects.create(
+        **{
+            "source": "WEB",
+            "province": data["province"],
+            "city": data["city"],
+            "age": data["age_range"],
+            "fever": "yes" == data["symptoms_fever"],
+            "cough": "yes" == data["symptoms_cough"],
+            "sore_throat": "yes" == data["symptoms_sore_throat"],
+            "difficulty_breathing": "yes" == data["symptoms_difficulty_breathing"],
+            "exposure": data["medical_exposure"],
+            "muscle_pain": "yes" == data["symptoms_muscles_hurt"],
+            "smell": "yes" == data["symptoms_taste"],
+            "preexisting_condition": data["symptoms_pre_existing_condition"],
+            "risk": risk_level,
+            "location": get_location(data),
+            "confirm_accuracy": "yes" == data["medical_confirm_accuracy"],
+        }
+    )
