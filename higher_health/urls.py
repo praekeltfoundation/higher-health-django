523fff3d6d76d505622a0c0548e1d3125051891c
Erik Harding <erik@praekelt.org>
2020-05-09 08:51:42 +0200

    redirect to different page on submit

--- higher_health/urls.py
+++ higher_health/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.healthcheck_questionnaire, name="healthcheck_questionnaire"),
+    path("receipt/", views.healthcheck_receipt, name="healthcheck_receipt"),
     path("login/", views.healthcheck_login, name="healthcheck_login"),
     path("terms/", views.healthcheck_terms, name="healthcheck_terms"),
 ]
--- higher_health/views.py
+++ higher_health/views.py
@@ -6,27 +6,31 @@ from .utils import get_risk_level, save_data
 
 
 def healthcheck_questionnaire(request):
-    submitted = False
     if request.method == "POST":
         form = HealthCheckQuestionnaire(request.POST)
         if form.is_valid():
             data = form.cleaned_data
             risk_level = get_risk_level(data)
             save_data(data, risk_level)
-            return HttpResponseRedirect(f"/?submitted=True&risk_level={risk_level}")
+
+            request.session['risk_level'] = risk_level
+            return HttpResponseRedirect("/receipt/")
         else:
             # TODO: need to handle this
             print(form.errors)
     else:
         form = HealthCheckQuestionnaire()
-        if "submitted" in request.GET:
-            submitted = True
 
-    return render(
-        request,
-        "healthcheck_questionnaire.html",
-        {"form": form, "submitted": submitted},
-    )
+    return render(request, "healthcheck_questionnaire.html", {"form": form})
+
+
+def healthcheck_receipt(request):
+    if request.session.has_key('risk_level'):
+        risk_level = request.session.get('risk_level')
+        del request.session['risk_level']
+        return render(request, "includes/receipt.html", {"risk_level": risk_level})
+    else:
+        return HttpResponseRedirect("/")
 
 
 def healthcheck_login(request):
