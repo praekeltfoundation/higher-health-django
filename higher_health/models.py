6b9047e6bebf9080a7940cdf9cc4e5ed47a45cab
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2020-05-14 13:53:55 +0200

    str representation

--- higher_health/models.py
+++ higher_health/models.py
@@ -99,7 +99,13 @@ class University(models.Model):
     name = models.CharField(max_length=100)
     province = models.CharField(choices=PROVINCE_CHOICES, max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Campus(models.Model):
     name = models.CharField(max_length=100)
     university = models.ForeignKey(University, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
