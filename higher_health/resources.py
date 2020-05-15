ec7c6e2874c27daaf48689359401f5faa29fd09b
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2020-05-15 13:48:49 +0200

    stub

--- higher_health/resources.py
+++ higher_health/resources.py
@@ -14,5 +14,18 @@ class CampusResource(resources.ModelResource):
 
     class Meta:
         model = models.Campus
-        fields = ('id', 'name', 'university__name')
+        fields = ('id', 'name', 'university__name', 'university_id')
+
+    def before_import(self, dataset, *args, **kwargs):
+        if 'id' not in dataset.headers:
+            dataset.insert_col('', lambda row: "", header='id')
+
+        if 'university_id' not in dataset.headers:
+            dataset.insert_col('', lambda row: "", header='university_id')
+
+        uni = models.University.objects.all()
+
+        for row in dataset.rows:
+            row['university_id'] = uni.filter(name=row['name']).first().pk
+
 
