4b93829ecdedb9822958f47e6e9d5b23a1a7e724
Jacques Coetzee <j.coetzee0@gmail.com>
2020-06-10 11:47:48 +0200

    remove local settings

--- higher_health/context_processors.py
+++ higher_health/context_processors.py
@@ -4,5 +4,6 @@ from django.conf import settings
 def api_keys(request):
     return {"PLACES_API_KEY": settings.CLIENT_PLACES_API_KEY}
 
+
 def ga_tags(request):
     return {"GA_TAG": settings.GA_TAG_KEY}
