00a2c3b454572f8411195ff16ade82ba1dbf7898
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2020-06-24 08:10:06 +0200

    black and flake8 changes

--- higher_health/tests/test_management_commands.py
+++ higher_health/tests/test_management_commands.py
@@ -8,48 +8,39 @@ class AdminViewsTest(TestCase):
     def test_create_dropdown_fields(self):
         uni1 = factories.UniversityFactory()
         camp1 = factories.CampusFactory(university=uni1)
-        factories.CampusFactory(
-            university=factories.UniversityFactory())
+        factories.CampusFactory(university=factories.UniversityFactory())
 
-        management.call_command('create_other_dropdown_field_values')
+        management.call_command("create_other_dropdown_field_values")
 
-        self.assertEqual(
-            uni1._meta.model.objects.all().count(),
-            3)
+        self.assertEqual(uni1._meta.model.objects.all().count(), 3)
 
-        self.assertEqual(
-            camp1._meta.model.objects.all().count(),
-            3)
+        self.assertEqual(camp1._meta.model.objects.all().count(), 3)
 
         self.assertEqual(
-            uni1._meta.model.objects.filter(name__iexact='Other').first().sort_order,
-            3)
+            uni1._meta.model.objects.filter(name__iexact="Other").first().sort_order, 3
+        )
 
         self.assertEqual(
-            camp1._meta.model.objects.filter(name__iexact='Other').first().sort_order,
-            3)
+            camp1._meta.model.objects.filter(name__iexact="Other").first().sort_order, 3
+        )
 
     def test_create_dropdown_fields_created(self):
         uni1 = factories.UniversityFactory()
         camp1 = factories.CampusFactory(university=uni1)
         factories.CampusFactory(
-            name="Other",
-            university=factories.UniversityFactory(name="Other"))
+            name="Other", university=factories.UniversityFactory(name="Other")
+        )
 
-        management.call_command('create_other_dropdown_field_values')
+        management.call_command("create_other_dropdown_field_values")
 
-        self.assertEqual(
-            uni1._meta.model.objects.all().count(),
-            2)
+        self.assertEqual(uni1._meta.model.objects.all().count(), 2)
 
-        self.assertEqual(
-            camp1._meta.model.objects.all().count(),
-            2)
+        self.assertEqual(camp1._meta.model.objects.all().count(), 2)
 
         self.assertEqual(
-            uni1._meta.model.objects.filter(name__iexact='Other').first().sort_order,
-            2)
+            uni1._meta.model.objects.filter(name__iexact="Other").first().sort_order, 2
+        )
 
         self.assertEqual(
-            camp1._meta.model.objects.filter(name__iexact='Other').first().sort_order,
-            2)
+            camp1._meta.model.objects.filter(name__iexact="Other").first().sort_order, 2
+        )
