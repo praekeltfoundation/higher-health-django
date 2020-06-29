a255c55bbf256b8ff9ae8f04dae58a314f8fcaa8
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2020-06-29 14:11:10 +0200

    update receipt template, tests and add expiry template filter

--- higher_health/templatetags/temp_tags.py
+++ higher_health/templatetags/temp_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils import timezone
 
 register = template.Library()
 
@@ -6,3 +7,11 @@ register = template.Library()
 @register.filter(name="field_type")
 def field_type(field):
     return field.field.widget.__class__.__name__
+
+
+@register.filter
+def expires(val, days, midnight=True):
+    expire = val + timezone.timedelta(days=days)
+    if midnight:
+        expire.replace(second=59, minute=59, hour=23)
+    return expire
