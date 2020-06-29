0742607e9f91d598ea4417f2e9ff378c35c1fd6a
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2020-06-29 17:11:04 +0200

    clean up db weird tests not truncating db after each test

--- higher_health/tests/test_management_commands.py
+++ higher_health/tests/test_management_commands.py
@@ -25,19 +25,32 @@ class AdminViewsTest(TestCase):
         )
 
     def test_create_dropdown_fields_created(self):
+        factories.UniversityFactory._meta.model.\
+            objects.all().delete()
+        factories.CampusFactory._meta.\
+            model.objects.all().delete()
+
         uni1 = factories.UniversityFactory()
         camp1 = factories.CampusFactory(university=uni1)
+
         factories.CampusFactory(
-            name="Other", university=factories.UniversityFactory(
+            name="Other",
+            university=factories.UniversityFactory(
                 name="Other", province='')
         )
 
         management.call_command("create_other_dropdown_field_values")
 
         self.assertEqual(
-            uni1._meta.model.objects.filter(name__iexact="Other").first().sort_order, 1
+            uni1._meta.model.objects.get(
+                name__iexact="Other",
+                province=''
+            ).sort_order, 1
         )
 
         self.assertEqual(
-            camp1._meta.model.objects.filter(name__iexact="Other").first().sort_order, 1
+            camp1._meta.model.objects.get(
+                name__iexact="Other",
+                university__name__iexact="Other",
+            ).sort_order, 1
         )
