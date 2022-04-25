f5246392a8ac7c26a9747b8ea15cb1d56521589c
Kaitlyn Crawford <kaitlyn@praekelt.com>
2022-04-25 15:36:14 +0200

    Linting changes

--- config/settings/base.py
+++ config/settings/base.py
@@ -93,13 +93,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASE_URL = env.db("DATABASE_URL", "")
 if not DATABASE_URL:
-    DATABASE_URL = env.db("HIGHER_HEALTH_DATABASE",
-        "postgres://postgres@localhost:5432/higher_health"
+    DATABASE_URL = env.db(
+        "HIGHER_HEALTH_DATABASE", "postgres://postgres@localhost:5432/higher_health"
     )
 
-DATABASES = {
-    "default": DATABASE_URL
-}
+DATABASES = {"default": DATABASE_URL}
 
 
 # Password validation
