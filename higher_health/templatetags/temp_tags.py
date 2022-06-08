195809b45c69349c0a4cb755632cbfc0a330c9be
Kaitlyn Crawford <kaitlyn@praekelt.com>
2022-06-09 00:43:12 +0200

    Add setting and template tag for maintenance mode

--- higher_health/templatetags/temp_tags.py
+++ higher_health/templatetags/temp_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
@@ -6,3 +7,8 @@ register = template.Library()
 @register.filter(name="field_type")
 def field_type(field):
     return field.field.widget.__class__.__name__
+
+
+@register.simple_tag
+def is_maintenance_mode():
+    return getattr(settings, "MAINTENANCE_MODE", False)
