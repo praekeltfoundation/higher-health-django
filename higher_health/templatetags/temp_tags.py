6f8a38fe2a9579dae02586a1139fa8b3d24c4e48
Kaitlyn Crawford <kaitlyn@praekelt.com>
2022-06-13 13:10:57 +0200

    Move maintenance mode to it's own page

--- higher_health/templatetags/temp_tags.py
+++ higher_health/templatetags/temp_tags.py
@@ -1,5 +1,4 @@
 from django import template
-from django.conf import settings
 
 register = template.Library()
 
@@ -7,8 +6,3 @@ register = template.Library()
 @register.filter(name="field_type")
 def field_type(field):
     return field.field.widget.__class__.__name__
-
-
-@register.simple_tag
-def is_maintenance_mode():
-    return getattr(settings, "MAINTENANCE_MODE", False)
